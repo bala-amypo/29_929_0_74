b24dfbd479272fd52bfc6d19c3916272920ff555
bala-amypo <bala@amypo.in>
2025-12-15 17:08:25 +0000

    User commit

--- src/main/java/com/example/demo/controller/LocationController.java
+++ src/main/java/com/example/demo/controller/LocationController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
-public class LocationController {
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class LocationController {
+    
 }
