4e97469141205276070dfe56d3ad3d94386f7ed6
bala-amypo <bala@amypo.in>
2025-12-17 07:40:14 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -2,13 +2,59 @@ package com.example.demo.entity;
 
 import java.util.Date;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 @Entity
 public class StudentEntity {
+    @Id
+    @GeneratedV
     private Integer id;
     private String username;
     private String email;
     private String password;
     private Date date;
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    public StudentEntity(Integer id, String username, String email, String password, Date date) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.date = date;
+    }
+    public StudentEntity() {
+    }
+
     
+
     
 }
